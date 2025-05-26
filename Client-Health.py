73a5542b5926b9c67efab95d667081851ce8ca55
Take-Off-7 <tfakeye7@gmail.com>
2025-05-26 14:54:42 +0100

    Removed plaintext password

--- Client-Health.py
+++ Client-Health.py
@@ -4,7 +4,7 @@ import json
 #################### LOGIN ####################
 url = "https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token"
 user = 'devnetuser'
-pw = 'Cisco123!'
+pw = #(password goes here)
 
 response = requests.post(
     url,
